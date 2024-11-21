328ba912f75bdded921190281514057c58401e4e
julio101290 <juliocesarleyvarodriguez@hotmail.com>
2024-11-21 09:42:43 -0700

    First upload

--- src/Commands/InstallCommandSettings.php
+++ src/Commands/InstallCommandSettings.php
@@ -4,11 +4,13 @@ namespace julio101290\boilerplatesettings\Commands;
 
 use CodeIgniter\CLI\BaseCommand;
 use Config\Database;
+use Config\Autoload;
+use CodeIgniter\CLI\CLI;
 
 /**
  * Class InstallCommand.
  */
-class InstallCommand extends BaseCommand
+class InstallCommandSettings extends BaseCommand
 {
     /**
      * The group the command is lumped under
@@ -23,7 +25,7 @@ class InstallCommand extends BaseCommand
      *
      * @var string
      */
-    protected $name = 'boilerplatesettings:install';
+    protected $name = 'boilerplatesettings:installsettings';
 
     /**
      * The command's short description.
@@ -63,6 +65,10 @@ class InstallCommand extends BaseCommand
     public function run(array $params)
     {
         try {
+            
+            $this->determineSourcePath();
+            
+            $this->publishMigration();
             $this->call('migrate');
             // then seed data
             $seeder = Database::seeder();
@@ -71,4 +77,58 @@ class InstallCommand extends BaseCommand
             $this->showError($e);
         }
     }
+    
+    protected function publishMigration()
+    {
+        $map = directory_map($this->sourcePath.'/Database/Migrations');
+
+        foreach ($map as $file) {
+            $content = file_get_contents("{$this->sourcePath}/Database/Migrations/{$file}");
+            //$content = str_replace('namespace julio101290\boilerplatesettings\Database\Migrations', 'namespace '.APP_NAMESPACE.'\Database\Migrations', $content);
+
+            $this->writeFile("Database/Migrations/{$file}", $content);
+        }
+    }
+    
+    
+     protected function determineSourcePath()
+    {
+        $this->sourcePath = realpath(__DIR__.'/../');
+
+        if ($this->sourcePath == '/' || empty($this->sourcePath)) {
+            CLI::error('Unable to determine the correct source directory. Bailing.');
+            exit();
+        }
+    }
+    
+        /**
+     * Write a file, catching any exceptions and showing a
+     * nicely formatted error.
+     *
+     * @param string $path
+     * @param string $content
+     */
+    protected function writeFile(string $path, string $content)
+    {
+        $config = new Autoload();
+        $appPath = $config->psr4[APP_NAMESPACE];
+
+        $directory = dirname($appPath.$path);
+
+        if (!is_dir($directory)) {
+            mkdir($directory, 0777, true);
+        }
+
+        try {
+            write_file($appPath.$path, $content);
+        } catch (\Exception $e) {
+            $this->showError($e);
+            exit();
+        }
+
+        $path = str_replace($appPath, '', $path);
+
+        CLI::write(CLI::color('  created: ', 'green').$path);
+    }
+    
 }
